a12fc0e54e801faebd3fc27a0c7da1db3d6326d1
Andrei Misiurov <andrey.misyrov@gmail.com>
2025-10-31 16:30:37 +0000

    refactor: migrate from character device to /proc interface

--- src/rpifan.c
+++ src/rpifan.c
@@ -7,6 +7,7 @@
 #include <linux/string.h>
 #include <linux/gpio.h>
 #include <linux/thermal.h>
+#include <linux/proc_fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrei Misiurov");
@@ -17,51 +18,22 @@ MODULE_VERSION("0.1.0");
 #define DEVICE_NAME "rpifan"
 #define CLASS_NAME "rpifan"
 
-static int major_num;
-static struct class *rpifan_class = NULL;
-static struct device *rpifan_device = NULL;
+static struct proc_dir_entry *rpifan_proc_dir = NULL;
+static struct proc_dir_entry *proc_status = NULL;
 
 static struct thermal_zone_device *cpu_thermal_zone = NULL;
 static unsigned int fan_enabled = 0;
 
-static ssize_t rpifan_write(struct file *file, const char __user *buf, size_t len, loff_t *offset);
-static ssize_t rpifan_read(struct file *file, char __user *buf, size_t len, loff_t *offset);
+static ssize_t status_proc_read(struct file *file, char __user *buf, size_t len, loff_t *offset);
+static ssize_t status_proc_write(struct file *file, const char __user *buf, size_t len, loff_t *offset);
 static int get_cpu_temp(void);
 
-static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .write = rpifan_write,
-    .read = rpifan_read,
+static const struct proc_ops status_proc_ops = {
+    .proc_read = status_proc_read,
+    .proc_write = status_proc_write,
 };
 
-static ssize_t rpifan_write(struct file *file, const char __user *buf, size_t len, loff_t *offset) {
-    char command[5] = {0};
-
-    if (len > 4) len = 4;
-    if (len == 0) return -EINVAL;
-
-    if (copy_from_user(command, buf, len))
-        return -EFAULT;
-
-    if (command[len - 1] == '\n')
-        command[len - 1] = '\0';
-
-    if (0 == strcmp(command, "on")) {
-        fan_enabled = 1;
-        gpio_set_value(FAN_GPIO, fan_enabled);
-        pr_info("%s: Fan turned ON\n", DEVICE_NAME);
-    } else if (0 == strcmp(command, "off")) {
-        fan_enabled = 0;
-        gpio_set_value(FAN_GPIO, fan_enabled);
-        pr_info("%s: Fan turned OFF\n", DEVICE_NAME);
-    } else {
-        pr_warn("%s: Unknown command: '%s'\n", DEVICE_NAME, command);
-        return -EINVAL;
-    }
-    return len;
-}
-
-static ssize_t rpifan_read(struct file *file, char __user *buf, size_t len, loff_t *offset) {
+static ssize_t status_proc_read(struct file *file, char __user *buf, size_t len, loff_t *offset) {
     char status[256] = {0};
     size_t status_len = 0;
     int temp = 0;
@@ -88,6 +60,33 @@ static ssize_t rpifan_read(struct file *file, char __user *buf, size_t len, loff
 
     *offset += status_len;
     return status_len;
+}
+
+static ssize_t status_proc_write(struct file *file, const char __user *buf, size_t len, loff_t *offset) {
+    char command[5] = {0};
+
+    if (len > 4) len = 4;
+    if (len == 0) return -EINVAL;
+
+    if (copy_from_user(command, buf, len))
+        return -EFAULT;
+
+    if (command[len - 1] == '\n')
+        command[len - 1] = '\0';
+
+    if (0 == strcmp(command, "on")) {
+        fan_enabled = 1;
+        gpio_set_value(FAN_GPIO, fan_enabled);
+        pr_info("%s: Fan turned ON\n", DEVICE_NAME);
+    } else if (0 == strcmp(command, "off")) {
+        fan_enabled = 0;
+        gpio_set_value(FAN_GPIO, fan_enabled);
+        pr_info("%s: Fan turned OFF\n", DEVICE_NAME);
+    } else {
+        pr_warn("%s: Unknown command: '%s'\n", DEVICE_NAME, command);
+        return -EINVAL;
+    }
+    return len;
 }
 
 static int get_cpu_temp(void) {
@@ -129,55 +128,46 @@ static int rpifan_gpio_init(void) {
 static int __init rpifan_init(void) {
     int ret = 0;
 
-    major_num = register_chrdev(0, DEVICE_NAME, &fops);
-    if (major_num < 0) {
-        pr_err("%s: Failed to register device: %d\n", DEVICE_NAME, major_num);
-        return major_num;
-    }
-
-    rpifan_class = class_create(THIS_MODULE, CLASS_NAME);
-    if (IS_ERR(rpifan_class)) {
-        ret = PTR_ERR(rpifan_class);
-        pr_err("%s: Failed to create device class\n", DEVICE_NAME);
-        goto err_chrdev;
-    }
-
-    rpifan_device = device_create(rpifan_class, NULL, MKDEV(major_num, 0), NULL, DEVICE_NAME);
-    if (IS_ERR(rpifan_device)) {
-        pr_err("%s: Failed to create device\n", DEVICE_NAME);
-        ret = PTR_ERR(rpifan_device);
-        goto err_class;
-    }
-
     ret = rpifan_gpio_init();
-    if (ret != 0) goto err_device;
+    if (ret != 0) return ret;
 
     cpu_thermal_zone = thermal_zone_get_zone_by_name("cpu-thermal");
     if (IS_ERR(cpu_thermal_zone)) {
         pr_err("%s: Failed to get thermal zone\n", DEVICE_NAME);
         ret = PTR_ERR(cpu_thermal_zone);
-        goto err_device;
+        goto err_gpio;
     }
 
-    pr_info("%s: Device created successfully (auto /dev/%s)\n", DEVICE_NAME, DEVICE_NAME);
-    return 0;
+    rpifan_proc_dir = proc_mkdir("rpifan", NULL);
+    if (!rpifan_proc_dir) {
+        pr_err("%s: Failed to create /proc/rpifan\n", DEVICE_NAME);
+        ret = -ENOMEM;
+        goto err_gpio;
+    }
 
-err_device:
-    device_destroy(rpifan_class, MKDEV(major_num, 0));
-err_class:
-    class_destroy(rpifan_class);
-err_chrdev:
-    unregister_chrdev(major_num, DEVICE_NAME);
+    proc_status = proc_create("status", 0444, rpifan_proc_dir, &status_proc_ops);
+    if (!proc_status) {
+        pr_err("%s: Failed to create /proc/rpifan/status\n", DEVICE_NAME);
+        ret = -ENOMEM;
+        goto err_proc;
+    }
+
+    pr_info("%s: /proc/rpifan created successfully\n", DEVICE_NAME);
+    return ret;
+
+err_proc:
+    proc_remove(rpifan_proc_dir);
+err_gpio:
+    gpio_free(FAN_GPIO);
     return ret;
 }
 
 static void __exit rpifan_exit(void) {
+    proc_remove(proc_status);
+    proc_remove(rpifan_proc_dir);
     gpio_set_value(FAN_GPIO, 0);
     gpio_free(FAN_GPIO);
-    device_destroy(rpifan_class, MKDEV(major_num, 0));
-    class_destroy(rpifan_class);
-    unregister_chrdev(major_num, DEVICE_NAME);
-    pr_info("%s: Device unregistered\n", DEVICE_NAME);
+    pr_info("%s: /proc/rpifan removed\n", DEVICE_NAME);
 }
 
 module_init(rpifan_init);
